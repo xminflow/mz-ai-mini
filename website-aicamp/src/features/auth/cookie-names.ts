da11c623c7aabfa04284c4e90fc5469fe9372d64
xmin <xminflow@gmail.com>
2026-06-11 23:36:10 +0800

    feat(aicamp-auth): cookie names (camp_*) and env conventions

---
+++ website-aicamp/src/features/auth/cookie-names.ts
@@ -0,0 +1,4 @@
+export const ACCESS_TOKEN_COOKIE = 'camp_access_token'
+export const REFRESH_TOKEN_COOKIE = 'camp_refresh_token'
+export const ACCESS_EXPIRES_COOKIE = 'camp_access_expires_at'
+export const REFRESH_EXPIRES_COOKIE = 'camp_refresh_expires_at'
